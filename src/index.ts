ca3f6f21dc80e97c97618f355147d0c77b6cd3b9
SoraKumo <info@croud.jp>
2019-11-17 22:42:32 +0900

    Test

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@ import * as core from "@actions/core";
 core.startGroup("GitHub Action Test");
 const message = core.getInput("message", { required: true });
 core.debug(message);
+core.debug("ふぉっふぉっふぉ");
+console.log("console");
 core.endGroup();
