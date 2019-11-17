8aac675a11d21f69889612695f65ce5af11efa34
SoraKumo <info@croud.jp>
2019-11-17 22:24:47 +0900

    テスト用フロー

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import * as core from "@actions/core";
-
-core.startGroup("GitHub Action Test");
-const message = core.getInput("message", { required: true });
-core.debug(message);
-core.endGroup();
